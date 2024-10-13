0ad17db798e0a062812ac1804b775d19c777c73c
YuHsuan <c.yuhsuann@gmail.com>
2024-10-13 17:23:00 +1000

    update checkpout/page --2

--- src/app/gallery/checkout/page.tsx
+++ src/app/gallery/checkout/page.tsx
@@ -1,15 +1,10 @@
 'use client'
 import { useEffect } from 'react';
-import { loadStripe } from '@stripe/stripe-js';
 import { Button } from '~/components/ui/button';
-
-// Make sure to call loadStripe outside of a component’s render to avoid
-// recreating the Stripe object on every render.
-const stripePromise = loadStripe(
-    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
-);
+import stripePromise from './lib/stripe';
 
 
+await stripePromise;
 
 export default function PreviewPage() {
     useEffect(() => {
@@ -25,6 +20,7 @@ export default function PreviewPage() {
     }, []);
 
     return (
+
         <form action="/api/checkout" method="POST">
             <section>
                 <Button type="submit" role="link">
@@ -32,7 +28,6 @@ export default function PreviewPage() {
                 </Button>
             </section>
         </form>
+
     );
 }
-
-export { stripePromise }
