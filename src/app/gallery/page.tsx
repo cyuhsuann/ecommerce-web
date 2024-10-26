17261efa1c211633a63103e0436de844254b86db
YuHsuan <c.yuhsuann@gmail.com>
2024-10-26 21:12:54 +1000

    restart again

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,13 +1,29 @@
 'use client';
 
-import { data } from "../products/[id]/products";
-import { CartSheet } from "../products/cartSheet";
+import { products } from "../products/[id]/products";
 import ProductItem from "../products/productItem";
 import { Provider, useAtom } from 'jotai'
 import { Product } from "../products/type";
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "~/components/ui/sheet";
+import Image from "next/image";
+import { Button } from "~/components/ui/button";
+import { Calculation } from "../tools/calculation";
+
 
 export default function Page() {
-    // const [products] = useAtom(data.products);
+    const [product, setProduct] = useAtom(products);
+    const stock = product.map(item => item.stock)
+    const { quantity, increaseQuantity, decreaseQuantity } = Calculation(10);
+    function addToCart() {
+        console.log("LALALALA~~~")
+    };
 
     return (
         <main>
@@ -18,14 +34,58 @@ export default function Page() {
             </div>
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 md:gap-8">
                 {/* NOTE: To show the each product */}
-                {data.products.map((product: Product) => {
+                {product.map((product: Product) => {
                     return (
                         <div key={product.id}>
                             <ProductItem product={product} />
-                            <CartSheet product={product} />
+                            <Button onClick={addToCart}>add to Cart</Button>
                         </div>
                     )
                 })}
+            </div>
+            <div>
+                {/* //
+                {for _ in list: <Component addItem={(quantity)=>addToCart(itemId, quantity)}></Component>}
+                {/* Inside component *}
+                <Button onClick={addItem(quantity)}></Button>
+                /* end inside component *}
+                // */}
+                <Sheet>
+                    <SheetTrigger>Show the Cart</SheetTrigger>
+                    <SheetContent>
+                        <SheetHeader>
+                            <SheetTitle>Cart</SheetTitle>
+                            <SheetDescription>Items in your cart</SheetDescription>
+                        </SheetHeader>
+                        {product.map(product => {
+                            return (
+                                <div key={product.id}>
+                                    <Image
+                                        src={product.image}
+                                        width={0}
+                                        height={0}
+                                        style={{ width: "120px", height: "auto" }}
+                                        priority={true}
+                                        alt={product.name}
+                                    />
+
+                                    <p>Item: {product.name}</p>
+                                    <p>Price: {product.price}</p>
+                                    <Button onClick={decreaseQuantity}> - </Button>
+                                    Quantity: {quantity}
+                                    <Button onClick={increaseQuantity}> + </Button>
+                                    <br />
+                                    <p>Total Price: ${quantity * product.price}</p>
+                                </div>
+                            )
+                        })}
+                        {/* IMPROTANT JST LEAVE THERE FROM NOW
+                        <form action="/api/checkout" method="POST">
+                            <Button>Checkout</Button>
+                        </form> */}
+
+                    </SheetContent>
+                </Sheet>
             </div>
         </main>
     )
--- src/app/products/[id]/products.ts
+++ src/app/products/[id]/products.ts
@@ -1,4 +1,4 @@
-import { atom, Atom } from "jotai";
+import { atom } from "jotai";
 
 export type productsType = {
     id: number;
@@ -7,64 +7,62 @@ export type productsType = {
     quantity: number
 }
 
-export const data = {
-    products: [
-        {
-            id: 1,
-            name: 'Tea Pot Set',
-            image: '/products/IMG_5331.png',
-            price: 300,
-            priceId: 'price_1QCFIuBmewR921WbydIvQebI',
-            stock: 10,
-            description: 'Artist-- YuChi'
-        },
-        {
-            id: 2,
-            name: 'Tea Cup * 4',
-            image: '/products/IMG_5332.png',
-            price: 120,
-            priceId: 'price_1QCFK1BmewR921WbGzRtnWkt',
-            stock: 20,
-            description: 'Artist-- YuChi'
-        },
-        {
-            id: 3,
-            name: 'Tea Pot',
-            image: '/products/IMG_5333.png',
-            price: 180,
-            priceId: 'price_1QCFKqBmewR921Wbzepc42Qm',
-            stock: 15,
-            description: 'Artist-- YuChi'
-        },
-        {
-            id: 4,
-            name: 'Tea Pot Set - Dark',
-            image: '/products/IMG_5331.png',
-            price: 320,
-            priceId: 'price_1QCFLCBmewR921WbClEVtBar',
-            stock: 16,
-            description: 'Artist-- YuChi'
-        },
-        {
-            id: 5,
-            name: 'Tea Cup * 8',
-            image: '/products/IMG_5332.png',
-            price: 200,
-            priceId: 'price_1QCFLvBmewR921WbkaimbwCt',
-            stock: 9,
-            description: 'Artist-- YuChi'
-        },
-        {
-            id: 6,
-            name: 'Tea Pot - Special',
-            image: '/products/IMG_5333.png',
-            price: 350,
-            priceId: 'price_1QCFMOBmewR921WbOEq8fCFh',
-            stock: 6,
-            description: 'Artist-- YuChi'
-        },
-    ]
-}
+export const products = atom([
+    {
+        id: 1,
+        name: 'Tea Pot Set',
+        image: '/products/IMG_5331.png',
+        price: 300,
+        priceId: 'price_1QCFIuBmewR921WbydIvQebI',
+        stock: 10,
+        description: 'Artist-- YuChi'
+    },
+    {
+        id: 2,
+        name: 'Tea Cup * 4',
+        image: '/products/IMG_5332.png',
+        price: 120,
+        priceId: 'price_1QCFK1BmewR921WbGzRtnWkt',
+        stock: 20,
+        description: 'Artist-- YuChi'
+    },
+    {
+        id: 3,
+        name: 'Tea Pot',
+        image: '/products/IMG_5333.png',
+        price: 180,
+        priceId: 'price_1QCFKqBmewR921Wbzepc42Qm',
+        stock: 15,
+        description: 'Artist-- YuChi'
+    },
+    {
+        id: 4,
+        name: 'Tea Pot Set - Dark',
+        image: '/products/IMG_5331.png',
+        price: 320,
+        priceId: 'price_1QCFLCBmewR921WbClEVtBar',
+        stock: 16,
+        description: 'Artist-- YuChi'
+    },
+    {
+        id: 5,
+        name: 'Tea Cup * 8',
+        image: '/products/IMG_5332.png',
+        price: 200,
+        priceId: 'price_1QCFLvBmewR921WbkaimbwCt',
+        stock: 9,
+        description: 'Artist-- YuChi'
+    },
+    {
+        id: 6,
+        name: 'Tea Pot - Special',
+        image: '/products/IMG_5333.png',
+        price: 350,
+        priceId: 'price_1QCFMOBmewR921WbOEq8fCFh',
+        stock: 6,
+        description: 'Artist-- YuChi'
+    },
+]);
 
 export const productPrice = atom({
     product1: 'price_1QCFIuBmewR921WbydIvQebI',
