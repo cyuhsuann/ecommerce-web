f2ff29ec1b124a27f3a21f8ee511bbbed23abf82
YuHsuan <c.yuhsuann@gmail.com>
2024-11-27 16:48:19 +1000

    modify the sheet

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,112 +1,129 @@
-'use client';
+"use client";
 
 import { products } from "../products/[id]/products";
 import ProductItem from "../products/productItem";
-import { atom, useAtom } from 'jotai'
+import { atom, useAtom } from "jotai";
 import type { CartItem, Checkout, Product } from "../products/type";
 import {
-    Sheet,
-    SheetContent,
-    SheetDescription,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger,
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
 } from "~/components/ui/sheet";
 import Image from "next/image";
 import { Button } from "~/components/ui/button";
 import { decreaseQuantity, increaseQuantity } from "../tools/calculation";
 
-
 const cartAtom = atom<CartItem[]>([]);
 
 export default function Page() {
-    const [product] = useAtom(products);
-    const [cart, setCart] = useAtom<CartItem[]>(cartAtom);
+  const [product] = useAtom(products);
+  const [cart, setCart] = useAtom<CartItem[]>(cartAtom);
 
-    async function sendToCheckout() {
-        const response = await fetch('/api/checkout', {
-            headers: { 'Content-Type': 'application/json' },
-            method: 'POST',
-            body: JSON.stringify({ cart })
-        })
-        console.log(response.ok)
-        if (!response.ok) {
-            console.error("Checkout request failed:", response.statusText);
-        };
-
-        const data: Checkout = await response.json() as Checkout;
-        if (data.url) {
-            // NOTE: Connect to the backend
-            window.location.href = data.url;
-        };
+  async function sendToCheckout() {
+    const response = await fetch("/api/checkout", {
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
+      body: JSON.stringify({ cart }),
+    });
+    console.log(response.ok);
+    if (!response.ok) {
+      console.error("Checkout request failed:", response.statusText);
     }
 
-    function addToCart(selectedItem: Product) {
-        setCart((prevCart: CartItem[]): CartItem[] => {
-            const existedItem = prevCart.find((item: CartItem) => item.product.id === selectedItem.id)
-            if (existedItem) {
-                return prevCart;
-            } else {
-                return [...prevCart, { product: selectedItem, quantity: 1 }]
-            }
-        })
-    };
+    const data: Checkout = (await response.json()) as Checkout;
+    if (data.url) {
+      // NOTE: Connect to the backend
+      window.location.href = data.url;
+    }
+  }
 
-    return (
-        <main>
-            <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-                <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-                    Pottery <span className="text-[hsl(280,100%,70%)]">Gallery</span>
-                </h1>
-            </div>
-            <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 md:gap-8">
-                {product.map((product: Product) => {
-                    return (
-                        <div key={product.id}>
-                            <ProductItem product={product} />
-                            <Button onClick={() => addToCart(product)}>add to Cart</Button>
-                        </div>
-                    )
-                })}
-            </div>
+  function addToCart(selectedItem: Product) {
+    setCart((prevCart: CartItem[]): CartItem[] => {
+      const existedItem = prevCart.find(
+        (item: CartItem) => item.product.id === selectedItem.id,
+      );
+      if (existedItem) {
+        console.log(prevCart);
+        return prevCart;
+      } else {
+        console.log(...prevCart, { product: selectedItem, quantity: 1 });
+        return [...prevCart, { product: selectedItem, quantity: 1 }];
+      }
+    });
+  }
+
+  return (
+    <main>
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+          Pottery <span className="text-[hsl(280,100%,70%)]">Gallery</span>
+        </h1>
+        <Sheet>
+          <SheetTrigger>Show Cart</SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>Cart</SheetTitle>
+              <SheetDescription>Items in your cart</SheetDescription>
+            </SheetHeader>
+            {cart.map((item: CartItem) => {
+              return (
+                <div key={item.product.id}>
+                  <Image
+                    src={item.product.image}
+                    width={0}
+                    height={0}
+                    style={{ width: "120px", height: "120px" }}
+                    priority={true}
+                    alt={item.product.name}
+                  />
+
+                  <p>Item: {item.product.name}</p>
+                  <p>
+                    Price: {item.product.price}
+                    <Button
+                      onClick={() =>
+                        decreaseQuantity(cart, setCart, item.product.id)
+                      }
+                    >
+                      {" "}
+                      -
+                    </Button>
+                    Quantity: {item.quantity}
+                    <Button
+                      onClick={() =>
+                        increaseQuantity(cart, setCart, item.product.id)
+                      }
+                    >
+                      {" "}
+                      +
+                    </Button>
+                  </p>
+                  <p>Subtotal: ${item.quantity * item.product.price}</p>
+                </div>
+              );
+            })}
+            <br />
+
             <div>
-                <Sheet>
-                    <SheetTrigger>Show Cart</SheetTrigger>
-                    <SheetContent>
-                        <SheetHeader>
-                            <SheetTitle>Cart</SheetTitle>
-                            <SheetDescription>Items in your cart</SheetDescription>
-                        </SheetHeader>
-                        here we are
-                        {cart.map((item: CartItem) => {
-                            return (
-                                <div key={item.product.id}>
-                                    <Image
-                                        src={item.product.image}
-                                        width={0}
-                                        height={0}
-                                        style={{ width: "120px", height: "auto" }}
-                                        priority={true}
-                                        alt={item.product.name}
-                                    />
-
-                                    <p>Item: {item.product.name}</p>
-                                    <p>Price: {item.product.price}
-                                        <Button onClick={() => decreaseQuantity(cart, setCart, item.product.id)}> - </Button>
-                                        Quantity: {item.quantity}
-                                        <Button onClick={() => increaseQuantity(cart, setCart, item.product.id)}> + </Button>
-                                    </p>
-                                    <p>Subtotal: ${item.quantity * item.product.price}</p>
-                                </div>
-                            )
-                        })}
-                        <br />
-                        <div>
-                            <Button onClick={sendToCheckout}>Checkout</Button>
-                        </div>
-                    </SheetContent>
-                </Sheet>
+              <Button onClick={sendToCheckout}>Checkout</Button>
             </div>
-        </main>
-    )
+          </SheetContent>
+        </Sheet>
+      </div>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 md:gap-8">
+        {product.map((product: Product) => {
+          return (
+            <div key={product.id}>
+              <ProductItem product={product} />
+              <Button onClick={() => addToCart(product)}>add to Cart</Button>
+            </div>
+          );
+        })}
+      </div>
+      <div></div>
+    </main>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,6 @@ export default async function Home() {
     void api.post.getLatest.prefetch();
   }
 
-
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
@@ -20,30 +19,7 @@ export default async function Home() {
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
             Pottery <span className="text-[hsl(280,100%,70%)]">Website</span>
           </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">First item →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">second item →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
-          </div>
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8"></div>
           <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
               {hello ? hello.greeting : "Loading tRPC query..."}
@@ -58,7 +34,8 @@ export default async function Home() {
                 // href={session ? "/api/auth/signout" : "/api/auth/signin"}
                 className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
               >
-                {session ? "Sign out" : "Sign in"}
+                {/* {session ? "Sign out" : "Sign in"} */}
+                Come Have a Look
               </Link>
             </div>
           </div>
