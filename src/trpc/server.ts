980b275237a5eb04aa37119099491bfcbe5d431f
YuHsuan <c.yuhsuann@gmail.com>
2025-07-24 20:12:06 +1000

    debug errors

--- src/trpc/server.ts
+++ src/trpc/server.ts
@@ -12,8 +12,9 @@ import { createQueryClient } from "./query-client";
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
  * handling a tRPC call from a React Server Component.
  */
-const createContext = cache(() => {
-  const heads = new Headers(headers());
+const createContext = cache(async () => {
+  const rawHeaders = await headers();
+  const heads = new Headers(rawHeaders);
   heads.set("x-trpc-source", "rsc");
 
   return createTRPCContext({
@@ -21,10 +22,9 @@ const createContext = cache(() => {
   });
 });
 
-const getQueryClient = cache(createQueryClient);
 const caller = createCaller(createContext);
 
 export const { trpc: api, HydrateClient } = createHydrationHelpers<AppRouter>(
   caller,
-  getQueryClient
+  createQueryClient,
 );
